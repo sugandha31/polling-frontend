9fbad96e23e46e48fd2e90f2d0a8e66e7e83ab41
Sugandha Bansal <bansal.sugandha@gmail.com>
2018-10-28 19:53:38 +0530

    update

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -4,20 +4,22 @@ import { Route } from '@angular/compiler/src/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from '../app.component';
 import { ProfileComponent } from '../profile/profile.component';
+import { CreatePollComponent } from '../create-poll/create-poll.component';
+import { QuestionComponent } from '../question/question.component';
 
 const appRoutes: Routes = [
   {
     path : 'profile',
     component : ProfileComponent
+  },
+  {
+    path : 'createPoll',
+    component : CreatePollComponent
+  },
+  {
+    path : 'question',
+    component : QuestionComponent
   }
-  // {
-  //   path : 'login',
-  //   component : LoginComponent  
-  // },
-  // {
-  //   path : 'signup',
-  //   component : SignupComponent
-  // }
 ];
 
 @NgModule({
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { trigger,state,style,transition,animate,keyframes } from '@angular/animations';
-
+import { NodeService } from './node.service';
+import { SESSION_STORAGE, StorageService } from 'angular-webstorage-service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  title = 'Polling System';
   collapsed = true;
   
+  constructor(private service: NodeService, @Inject(SESSION_STORAGE) private storage: StorageService) {}
+
   home : any;
   create_poll : any;
   live_poll : any;
   profile : any;
   faq : any;
   contact : any;
+  show : boolean;
+  Question: String[] = [];
 
-  name : any;
-  userid : any;
-  email : any;
-  password : any;
+  ngOnInit(){
+    if(this.storage.get('show')==true){
+      this.show = false;
+      this.getAll();
+    }else{
+      this.show = true;
+      this.getAll();
+    }
+  }
+
+  getAll(){
+    this.service.getAllPolls().subscribe((result: any)=>{
+      result.data.forEach(element => {
+        this.Question.push(element['poll']);
+      });
+      console.log(this.Question);
+    })
+  }
+
+  pgReload(){
+    window.location.reload(true);
+  }
 
   toggleCollapsed(): void {
     this.collapsed = !this.collapsed;
@@ -41,7 +65,40 @@ export class AppComponent {
     }
   }
 
-  login(email:any, password:any){
-    console.log(email+"---"+password);
+  login(email:String, password:String){
+    this.service.login(email, password).subscribe((result: any)=>{
+      if(result.status == true){
+        this.storage.set('show', true );
+        this.storage.set('user', {
+          'email': email,
+          'password': password
+        });
+        console.log(this.storage.get('show'));
+        console.log(this.storage.get('user'));
+        this.show = false;
+      }else{
+        this.storage.set('show', false );
+        console.log(this.storage.get('show'));
+        this.show = true;
+      }
+    })
+  }
+
+  signup(name: String, user_name: String, email: String, password: String){
+    this.service.signup(email, name, user_name, password).subscribe((result: any)=>{
+      console.log(result);
+      alert("You are successfully SignedUp. Login to proceed.");
+    })
+  }
+
+  logout(email: String){
+    this.service.logout(email).subscribe((result:any)=>{
+      if(result.status == true){
+        this.storage.set('show', false );
+        console.log(this.storage.get('show'));
+        alert('You are successfully logged out.');
+        this.show = true;
+      }
+    });
   }
 }
